cb9be8a2b8cff972711eecf36f938c0b9b3bc047
wahyu firmansyah <wahyu.firmansyah082@gmail.com>
2017-12-10 17:14:26 +0700

    authentication

---
+++ app/src/main/java/com/project/dian/mytodolist/fragment/AuthenticationFragment.java
@@ -0,0 +1,52 @@
+package com.project.dian.mytodolist.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.project.dian.mytodolist.R;
+
+
+public class AuthenticationFragment extends Fragment {
+
+
+    public AuthenticationFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment AuthenticationFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static AuthenticationFragment newInstance(String param1, String param2) {
+        AuthenticationFragment fragment = new AuthenticationFragment();
+        Bundle args = new Bundle();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_authentication, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/project/dian/mytodolist/fragment/LoginFragment.java
@@ -0,0 +1,44 @@
+package com.project.dian.mytodolist.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.project.dian.mytodolist.R;
+
+
+public class LoginFragment extends Fragment {
+
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static LoginFragment newInstance() {
+        LoginFragment fragment = new LoginFragment();
+        Bundle args = new Bundle();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+}
